715f36b31becb62b23eb3ede7da721d69f7c1264
Katherine Elms <kelms@augusta.edu>
2026-05-01 09:47:43 -0400

    scores done

--- scores_03/scores.py
+++ scores_03/scores.py
@@ -13,12 +13,19 @@ def filter_scores(scores: list[int]) -> list[int]:
     # Scores should be greater than or equal to 70
     # Scores should be less than or equal to 90
     # Scores should be even numbers
+    for score in scores:
+        if 70 <= score <= 90:
+            if score % 2 == 0:
+                filtered_scores.append(score)
+
 
     # Step 2: Sort scores from highest to lowest
     # Use the sort function with the reverse parameter
+    filtered_scores.sort()
+    filtered_scores.reverse()
 
     # Step 3: Return the filtered and sorted list
-
+    return filtered_scores
 
 def main():
     """DO NOT MODIFY THIS FUNCTION"""
